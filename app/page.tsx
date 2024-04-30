deff24e79764d00c3523ab7c0731a3d43aab4334
Gabriel Enrique <ged.venegas@gmail.com>
2024-04-30 12:07:34 -0300

    minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
-import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
-import {
-  FolderOpenDot,
-  LayersIcon,
-  RssIcon,
-  ShieldIcon,
-  StarIcon,
-} from "lucide-react";
+import { Avatar, AvatarFallback } from "../components/ui/avatar";
+import { FolderOpenDot, RssIcon, ShieldIcon, StarIcon } from "lucide-react";
 
 import { Badge } from "../components/ui/badge";
+import { Button } from "../components/ui/button";
+import Image from "next/image";
+import { Input } from "../components/ui/input";
 import Link from "next/link";
 import { currentUser } from "@clerk/nextjs/server";
+import githubImage from "../public/github-horizontal.webp";
+import notionImage from "../public/notion-horizontal.webp";
 
 export default async function Home() {
   const user = await currentUser();
@@ -31,26 +30,26 @@ export default async function Home() {
         <div className="container space-y-4 px-4 md:px-6">
           <div className="md:text-center space-y-8">
             <h2 className="text-6xl xl:text-7xl">
-              Centralize Your Developer Portfolio in One Powerful Link
+              Concentre Seu Portfólio em Um Único Link Dinâmico
             </h2>
 
-            <p className="w-5/6 lg:text-lg">
-              With OneLnk, consolidate your online presence into a single,
-              dynamic link. Designed for software developers, OneLnk lets you
-              showcase your GitHub projects, blog posts, and professional
-              networks—all in one spot.
+            <p className="md:m-auto w-5/6 lg:text-lg">
+              Especializado para desenvolvedores, centralize projetos do GitHub,
+              blogs e redes em um só lugar. Destaque-se e conecte-se na
+              comunidade tech
             </p>
 
-            {/* <div className="flex flex-row justify-center space-x-6">
-              <Input
-                className="max-w-md text-base"
-                placeholder="onelnk.pro/to/yourname"
-              />
-
+            <div className="flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-2">
+              <div className="flex flex-row items-center space-x-2">
+                <span className="md:text-lg font-semibold">
+                  https://onelnk.pro/to/
+                </span>
+                <Input className="text-base" placeholder="john-doe" />
+              </div>
               <Button className="transition-colors duration-500">
-                Claim you free link
+                Criar seu link grátis
               </Button>
-            </div> */}
+            </div>
           </div>
         </div>
       </section>
@@ -60,11 +59,12 @@ export default async function Home() {
           <div className="text-center space-y-4">
             <div className="space-y-2">
               <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-                Showcase, Share, and Connect Like Never Before
+                Destaque-se, Compartilhe e Conecte-se
               </h2>
               <p className="max-w-[700px] mx-auto text-gray-500 md:text-xl dark:text-gray-400">
-                Expand your developer network. Showcase projects, share
-                insights, and connect with tech professionals effortlessly.
+                Expanda sua rede de desenvolvedores. Apresente projetos,
+                compartilhe insights e conecte-se com profissionais de
+                tecnologia sem esforço
               </p>
             </div>
           </div>
@@ -73,55 +73,56 @@ export default async function Home() {
               <div className="flex space-x-2 items-center mb-4">
                 <FolderOpenDot className="h-8 w-8 text-primary" />
                 <h3 className="ml-4 text-lg font-semibold">
-                  Project Showcases
+                  Vitrine de Projetos
                 </h3>
-                <Badge>Soon</Badge>
+                <Badge>em breve</Badge>
               </div>
               <p className="text-gray-500 dark:text-gray-400">
-                Embed live GitHub repositories, display real-time stats, and
-                show off your development prowess with CI/CD status badges.
+                Mostre seus repositórios GitHub em tempo real, exiba
+                estatísticas atualizadas e evidencie sua expertise com insígnias
+                de status de CI/CD.
               </p>
             </div>
             <div className="bg-white dark:bg-gray-950 p-6 rounded-lg shadow-sm">
               <div className="flex space-x-2 items-center mb-4">
                 <RssIcon className="h-8 w-8 text-primary" />
                 <h3 className="ml-4 text-lg font-semibold">
-                  Integrated Blogging
+                  Blogging Integrado
                 </h3>
-                <Badge>Soon</Badge>
+                <Badge>em breve</Badge>
               </div>
               <p className="text-gray-500 dark:text-gray-400">
-                Automatically update your profile with your latest blog posts,
-                keeping your audience informed and engaged.
+                Seu perfil se atualiza automaticamente com as últimas postagens
+                do seu blog, mantendo seu público sempre informado e engajado.
               </p>
             </div>
             <div className="bg-white dark:bg-gray-950 p-6 rounded-lg shadow-sm">
               <div className="flex space-x-2 items-center mb-4">
                 <ShieldIcon className="h-8 w-8 text-primary" />
                 <h3 className="ml-4 text-lg font-semibold">
-                  Developer-Focused Networking
+                  Networking Focado em Desenvolvedores
+                  <Badge className="ml-2 mb-2">em breve</Badge>
                 </h3>
-                <Badge>Soon</Badge>
               </div>
               <p className="text-gray-500 dark:text-gray-400">
-                Direct links to your professional profiles on Stack Overflow,
-                LinkedIn, and more, making it easier to connect with the tech
-                community.
+                Acesso direto aos seus perfis profissionais no Stack Overflow,
+                LinkedIn e outros, simplificando sua conexão com a comunidade de
+                tecnologia.
               </p>
             </div>
-            <div className="bg-white dark:bg-gray-950 p-6 rounded-lg shadow-sm">
+            {/* <div className="bg-white dark:bg-gray-950 p-6 rounded-lg shadow-sm">
               <div className="flex space-x-2 items-center mb-4">
                 <LayersIcon className="h-8 w-8 text-primary" />
                 <h3 className="ml-4 text-lg font-semibold">
-                  Enhanced Visibility
+                  Visibilidade Aprimorada
                 </h3>
-                <Badge>Soon</Badge>
+                <Badge>em breve</Badge>
               </div>
               <p className="text-gray-500 dark:text-gray-400">
-                With SEO optimization, make it easier for potential employers,
-                collaborators, and followers to find you online.
+                Com otimização SEO, amplie suas chances de ser encontrado por
+                empregadores, colaboradores e seguidores na web.
               </p>
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
@@ -131,11 +132,38 @@ export default async function Home() {
           <div className="text-center space-y-4">
             <div className="space-y-2">
               <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-                What Our Customers Say
+                Integrações
               </h2>
               <p className="max-w-[700px] mx-auto text-gray-500 md:text-xl dark:text-gray-400">
-                Hear from our satisfied customers about their experience with
-                our products and services.
+                Aproveite nossas integrações para criar um perfil ainda mais
+                completo
+              </p>
+            </div>
+          </div>
+          <div className="grid gap-6 md:grid-cols-2 justify-items-center lg:grid-cols-2 mt-12">
+            <Image
+              src={githubImage}
+              className="h-[160px] w-[434px]"
+              alt="Github logo"
+            />
+            <Image
+              src={notionImage}
+              className="h-[160px] w-[300px]"
+              alt="Notion logo"
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
+        <div className="container px-4 md:px-6">
+          <div className="text-center space-y-4">
+            <div className="space-y-2">
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+                O Que Nossos Usuários Dizem
+              </h2>
+              <p className="max-w-[700px] mx-auto text-gray-500 md:text-xl dark:text-gray-400">
+                Veja o que nossos usuários têm a dizer sobre suas experiências
               </p>
             </div>
           </div>
@@ -157,57 +185,10 @@ export default async function Home() {
                 </div>
               </div>
               <p className="text-gray-500 dark:text-gray-400">
-                I've been using the product for a few months now and it's been a
-                game-changer for my business. The features are incredibly useful
-                and the support team has been amazing.
+                Desde que comecei a utilizar o OneLnk, compartilhar meus perfis
+                profissionais e mostrar meu trabalhou ficou bem mais fácil!
               </p>
             </div>
-            {/* <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-sm">
-              <div className="flex items-start mb-4">
-                <Avatar className="w-10 h-10 border">
-                  <AvatarImage alt="@shadcn" src="/placeholder-user.jpg" />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <div className="ml-4">
-                  <h3 className="font-semibold">Alex Smith</h3>
-                  <div className="flex items-center gap-0.5 mt-1">
-                    <StarIcon className="w-5 h-5" fill="black" />
-                    <StarIcon className="w-5 h-5" fill="black" />
-                    <StarIcon className="w-5 h-5" fill="black" />
-                    <StarIcon className="w-5 h-5" fill="black" />
-                    <StarIcon className="w-5 h-5" fill="black" />
-                  </div>
-                </div>
-              </div>
-              <p className="text-gray-500 dark:text-gray-400">
-                The platform is incredibly user-friendly and the onboarding
-                process was a breeze. I was able to get my business up and
-                running in no time.
-              </p>
-            </div>
-            <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-sm">
-              <div className="flex items-start mb-4">
-                <Avatar className="w-10 h-10 border">
-                  <AvatarImage alt="@shadcn" src="/placeholder-user.jpg" />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <div className="ml-4">
-                  <h3 className="font-semibold">Emily Parker</h3>
-                  <div className="flex items-center gap-0.5 mt-1">
-                    <StarIcon className="w-5 h-5" fill="black" />
-                    <StarIcon className="w-5 h-5" fill="black" />
-                    <StarIcon className="w-5 h-5" fill="black" />
-                    <StarIcon className="w-5 h-5" fill="black" />
-                    <StarIcon className="w-5 h-5" fill="black" />
-                  </div>
-                </div>
-              </div>
-              <p className="text-gray-500 dark:text-gray-400">
-                The customer support team has been incredibly responsive and
-                helpful. They've gone above and beyond to ensure I have a great
-                experience with the product.
-              </p>
-            </div> */}
           </div>
         </div>
       </section>
